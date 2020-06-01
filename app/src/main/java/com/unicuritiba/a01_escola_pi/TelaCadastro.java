46d1f049d03998d7d6c204e5b69fdeff7a6d00c3
Hedward Django Cordeiro <trab.hedward@gmail.com>
2020-06-01 13:34:39 -0300

    Criada a Tela de Cadastro.

---
+++ app/src/main/java/com/unicuritiba/a01_escola_pi/TelaCadastro.java
@@ -0,0 +1,14 @@
+package com.unicuritiba.a01_escola_pi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- app/src/main/java/com/unicuritiba/a01_escola_pi/main_activity/MainActivity.java
+++ app/src/main/java/com/unicuritiba/a01_escola_pi/main_activity/MainActivity.java
@@ -1,19 +1,20 @@
 package com.unicuritiba.a01_escola_pi.main_activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.textfield.TextInputEditText;
 import com.unicuritiba.a01_escola_pi.R;
-import com.unicuritiba.a01_escola_pi.tela_responsavel.activity.TelaResponsavel;
+import com.unicuritiba.a01_escola_pi.TelaCadastro;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText editCPF;
+    private TextInputEditText editSenha;
     private Button btnLogin;
 
     @Override
@@ -23,14 +24,24 @@ public class MainActivity extends AppCompatActivity {
 
 
         editCPF = findViewById(R.id.txtInputCPF);
+        editSenha = findViewById(R.id.txtnputSenha);
         btnLogin = findViewById(R.id.btnEntrar);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), TelaResponsavel.class);
+                Class classeResponsalvel = TelaCadastro.class;
+/*
+                if (editCPF.getText().toString().isEmpty() && editSenha.getText().toString().isEmpty()) {
+                    classeResponsalvel = TelaEscola.class;
+                } else if (editCPF.getText().toString() != null && editSenha.getText().toString() != null ) {
+                    classeResponsalvel = TelaCadastro.class;
+                }   //else if (editCPF.getText().toString().isEmpty() || editSenha.getText().toString().isEmpty()){
+                    //classeResponsalvel = TelaResponsavel.class;}
+*/
 
+                Intent intent = new Intent(getApplicationContext(), classeResponsalvel);
                 //Passar dados
                 startActivity(intent);
             }
